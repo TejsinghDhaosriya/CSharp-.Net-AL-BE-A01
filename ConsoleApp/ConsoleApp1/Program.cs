203c6eca1e073c54abb9bcd121bc43f764bdb768
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2022-06-29 21:38:59 +0530

    console app:

---
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibraryApp;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo ob = new Demo();
+            ob.message();
+        }
+    }
+}
